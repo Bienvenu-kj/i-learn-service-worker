9c8b0ddd1cf3f2780d4ffdb6bccb22af0bd5e3e8
Bienvenu Kajirifo Luc <bienvenukajirifoluc@gmail.com>
2025-04-12 20:14:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js')
     .then(reg => console.log('Service Worker enregistré', reg))
     .catch(err => console.error('Erreur SW', err));
-}
+}*/
