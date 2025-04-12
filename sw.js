3fade32e9a3043acf5a990ae109974266c8ec20d
Bienvenu Kajirifo Luc <bienvenukajirifoluc@gmail.com>
2025-04-12 18:21:16 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'mon-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installation...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[Service Worker] Mise en cache des fichiers...');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('[Service Worker] Interception de :', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
