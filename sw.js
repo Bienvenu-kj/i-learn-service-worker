525371427ca72c01946f0f1ee825813b74196067
Bienvenu Kajirifo Luc <bienvenukajirifoluc@gmail.com>
2025-04-12 20:28:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'mon-cache-v1';
 const FILES_TO_CACHE = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/script.js'
+  'index.html',
+  'style.css',
+  'script.js'
 ];
 
 self.addEventListener('install', event => {
